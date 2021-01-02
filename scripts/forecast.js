9b44083bc9c8f8c64d6cd1f0ad7638d49fc841d7
saidursikder <syrahman92@gmail.com>
2021-01-02 00:49:34 +0000

    initial deployment

---
+++ scripts/forecast.js
@@ -0,0 +1,26 @@
+class Forecast {
+    constructor() {
+        this.key = 'EM983CEgfzdn6CGGwFgPWmMtp6bDq9ve';
+        this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
+        this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    }
+    async updateCity(city) {
+        const cityDets = await this.getCity(city);
+        const weather = await this.getWeather(cityDets.Key);
+        return { cityDets, weather };
+    }
+    async getCity(city) {
+        const query = `?apikey=${this.key}&q=${city}`;
+        const response = await fetch(this.cityURI + query);
+        const data = await response.json();
+        return data[0];
+    }
+    async getWeather(id) {
+        const query = `${id}?apikey=${this.key}`;
+        const response = await fetch(this.weatherURI + query);
+        const data = await response.json();
+        return data[0];
+    }
+}
+
+
